38ba3c495a49594c02fbab10f8d016c20a288487
pebriniel <zeldalyoko@gmail.com>
2020-01-05 06:23:59 +0100

    ajout des options pour Twig dans app.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const Twig = require("twig");
 const express = require('express');
 const indexRoute = require('./src/routes/index.js');
+const connexionRoute = require('./src/routes/connexion.js');
 
 const app = express();
 
+app.set("twig options", {
+    allow_async: true, // Allow asynchronous compiling
+    strict_variables: false
+});
+
 app.get('/', indexRoute);
+app.get('/login', connexionRoute);
 
 app.listen(8000);
